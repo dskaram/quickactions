10bcd959c6675c4e0675111c6e506d4883535d5a
David Karam <dskaram@gmail.com>
2015-02-21 11:04:01 +0100

    Use Bindings.bindWithHandler to ensure initialization while binding

--- quickaction/js/view/QuickActionView.js
+++ quickaction/js/view/QuickActionView.js
@@ -1,6 +1,7 @@
 define([
   "underscore",
   "backbone",
+  "util/Bindings",
   "view/Keys",
   "view/Matcher",
   "view/Selection",
@@ -11,6 +12,7 @@ define([
   ], function(
     _,
     Backbone,
+    Bindings,
     Keys,
     Matcher,
     Selection,
@@ -43,16 +45,34 @@ define([
 
     initialize: function(options) {
       this.layers= options.layers;
+      this.viewModel= options.viewModel;
+    },
 
+    render: function() {
+      this.$el.html(QuickActionViewTemplate({}));
+
+      this.inputBox= this.$el.find(".quick-actions-search-group input");
+      this.providerPlaceholder= this.$el.find(".quick-actions-search-group .quick-actions-search-group-addon");
+      this.listView= this.$el.find(".quick-actions-list-view");
+
+      this._bindViewModel();
+      return this;
+    },
+
+    focus: function() {
+      this.inputBox.focus();
+    },
+
+    _bindViewModel: function() {
       var self= this;
 
-      options.viewModel.on("change:breadcrumb", function(model, breadcrumbs) {
+      Bindings.bind(this.viewModel, "breadcrumb", function(model, breadcrumbs) {
         var node= self.$el.find(".quick-actions-breadcrumb");
         node.html(Breadcrumbs(breadcrumbs));
         node.children().last().addClass("quick-actions-active");
       });
 
-      options.viewModel.on("change:open", function(model, open) {
+      Bindings.bind(this.viewModel, "open", function(model, open) {
         self.$el.toggleClass("quick-actions-closed", !open);
       });
 
@@ -116,20 +136,6 @@ define([
         removed.css("transform", "translateX(" + 100 + "%)");
           // destroy all listeners
       });
-
-    },
-
-    render: function() {
-      this.$el.html(QuickActionViewTemplate({}));
-
-      this.inputBox= this.$el.find(".quick-actions-search-group input");
-      this.providerPlaceholder= this.$el.find(".quick-actions-search-group .quick-actions-search-group-addon");
-      this.listView= this.$el.find(".quick-actions-list-view");
-      return this;
-    },
-
-    focus: function() {
-      this.inputBox.focus();
     },
 
     _onKeyDown: function(e) {
